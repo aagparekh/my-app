84132171df689d05821382c54a78f5d249b514ce
aagparekh <89527200+aagparekh@users.noreply.github.com>
2022-05-23 17:13:36 +0530

    change

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
-import About from './components/About';
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes
-} from "react-router-dom";
+// import About from './components/About';
+// import {
+//   BrowserRouter as Router,
+//   Route,
+//   Routes
+// } from "react-router-dom";
 
 
 function App() {
@@ -25,21 +25,23 @@ function App() {
     }, 3000);
   }
   return (
-    <><Router>
+    <>
+    {/* <Router> */}
       {/* <Navbar title='TextUtils' link='Link with us'/> */}
       <Navbar />
       <Alert alert={alert} />
       <div className="container">
-
+{/* 
         <Routes>
           <Route path="/about" element={<About />}>
-          </Route>
-          <Route path="/" element={<TextForm heading='Enter Text Here' showAlert={showAlert} />}>
-          </Route>
-        </Routes>
+          </Route> */}
+          {/* <Route path="/" element={<TextForm heading='Enter Text Here' showAlert={showAlert} />}>
+          </Route> */}
+          <TextForm heading='Enter Text Here' showAlert={showAlert}/>
+        {/* </Routes> */}
 
       </div>
-    </Router>
+    {/* </Router> */}
     </>
   );
 }
